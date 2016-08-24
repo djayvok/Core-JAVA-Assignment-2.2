c5d3d54a0b8a5e069616c99f358b7f0b6f5f258a
Dhananjaya <djay.vok@gmail.com>
2016-08-24 10:44:01 +0100

    Update Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -6,7 +6,7 @@ public class Pattern
         for (int i=0;i<size;i++){
             for(int j=0;j<size;j++){
                 if(i==j||(i+j)==size-1){
-                    System.out.print("x");
+                    System.out.print("*");
                 }
                 else{
                     System.out.print("   ");
